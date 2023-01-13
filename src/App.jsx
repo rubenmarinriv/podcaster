90fc9ec92e8f5f4113594922930acd98da30925c
Rubén Marín Rivarés <rubenmarinriv@gmail.com>
2023-01-13 15:50:26 +0100

    [main-view] refactor: move fetchFeed inside the useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,17 @@ import './App.css';
 function App() {
   const [podcasts, setPodcasts] = useState(null);
 
-  const fetchFeed = async () => {
-    const { feed } = await (
-      await fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
-    ).json();
-
-    setPodcasts(feed.entry);
-  };
-
   useEffect(() => {
-    if (podcasts === null) {
-      fetchFeed();
-    }
-  });
+    const fetchFeed = async () => {
+      const { feed } = await (
+        await fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
+      ).json();
+
+      setPodcasts(feed.entry);
+    };
+
+    fetchFeed();
+  }, []);
 
   return (
     <div className="App">
